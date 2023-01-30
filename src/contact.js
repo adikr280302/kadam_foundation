05a0bf2533de80852a1c7ebcca735e56081cc944
Aditya Kumar <kradi@amazon.com>
2023-01-31 02:51:59 +0530

    done

--- src/contact.js
+++ src/contact.js
@@ -1,21 +1,7 @@
 import React from 'react'
 import "./Contact.css"
-import {useState, useEffect} from "react"
 
 function Contact() {
-  const [text, setText] = useState("");
-  const [fullText, setFullText] = useState(
-    "Kadam Foundation is an NGO established in 2015. We have been helping needy children since then by providing food, clothes, and other helpful things. We stood strong in difficult situations like COVID-19 and did continuous service. We believe your support would help us to create a better world as children are our future."
-  );
-  const [index, setIndex] = useState(0);
-  useEffect(() => {
-    if (index < fullText.length) {
-      setTimeout(() => {
-        setText(text + fullText[index]);
-        setIndex(index + 1);
-      }, 40)
-    }
-  }, [index]);
   return (
     <>
       <h2>
